50aa7c12d4a159329806664f4226ac3acdafd4e8
Ragavaa M <136820624+Ragav-CR7@users.noreply.github.com>
2026-05-24 11:29:53 +0530

    Update Vite config to include Nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({ adapter: "vercel" }),
+    tanstackStart(),
+    nitro({
+      preset: "vercel",
+    }),
     react(),
     tsconfigPaths(),
   ],
